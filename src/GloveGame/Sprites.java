5e5411461fc87a43ff87a8e54d5d08945992fce0
tylerc112 <caudell.tyler@gmail.com>
2015-02-28 00:33:38 -0600

    Working on a Sprite class.

---
+++ src/GloveGame/Sprites.java
@@ -0,0 +1,28 @@
+package GloveGame;
+
+import java.awt.image.BufferedImage;
+
+/**
+ *
+ * @author Tyler
+ */
+public class Sprites {
+    
+    private BufferedImage img;
+    private SpriteList selectedSprites;
+    private SpriteState 
+    
+    public enum SpriteList {
+        TODO,
+    }
+    
+    public enum SpriteState {
+        TODO,
+    }
+    
+    public Sprites(SpriteList sprite) {
+        
+    }
+}
+
+
