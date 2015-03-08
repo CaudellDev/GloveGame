c1e566d55bcbb27de720a2bf5a7ea62df380b565
tylerc112 <caudell.tyler@gmail.com>
2015-03-07 22:54:11 -0600

    LWJGL base started.

---
+++ src/GloveGame/LWJGLMain.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GloveGame;
+
+import java.awt.DisplayMode;
+import org.lwjgl.system.windows.DISPLAY_DEVICE;
+
+/**
+ *
+ * @author Tyler
+ */
+public class LWJGLMain implements Runnable {
+    
+    private boolean running = false;
+    private Thread thread;
+    private long window;
+    
+    public void start() {
+        
+    }
+    
+    public void run() {
+        
+    }
+    
+    private void init() {
+        
+    }
+    
+    private void update() {
+        
+    }
+    
+    private void render() {
+        
+    }
+    
+    public static void main(String[] args) {
+        new LWJGLMain().start();
+    }
+}
