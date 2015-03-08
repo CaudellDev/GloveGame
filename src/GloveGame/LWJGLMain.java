4995ee989718ea848fd1733c8a0f0a702ea42ed1
tylerc112 <caudell.tyler@gmail.com>
2015-03-08 01:36:53 -0600

    Window with black background now shows.

--- src/GloveGame/LWJGLMain.java
+++ src/GloveGame/LWJGLMain.java
@@ -5,8 +5,11 @@
  */
 package GloveGame;
 
-import java.awt.DisplayMode;
-import org.lwjgl.system.windows.DISPLAY_DEVICE;
+import java.nio.ByteBuffer;
+import static org.lwjgl.glfw.GLFW.*;
+import org.lwjgl.glfw.GLFWvidmode;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryUtil.*;
 
 /**
  *
@@ -14,28 +17,57 @@ import org.lwjgl.system.windows.DISPLAY_DEVICE;
  */
 public class LWJGLMain implements Runnable {
     
+    private int width = 700;
+    private int height = 600;
+    
     private boolean running = false;
     private Thread thread;
     private long window;
     
     public void start() {
-        
+        running = true;
+        thread = new Thread(this, "Game");
+        thread.start();
     }
     
     public void run() {
-        
+        init();
+        while (running) {
+            update();
+            render();
+            
+            if (glfwWindowShouldClose(window) == GL_TRUE) {
+                running = false;
+            }
+        }
     }
     
     private void init() {
+        if (glfwInit() != GL_TRUE) {
+            // TODO: handle it
+            return;
+        }
         
+        glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
+        window = glfwCreateWindow(width, height, "Glove", NULL, NULL);
+        
+        if (window == NULL) {
+            // TODO: handle
+            return;
+        }
+        
+        ByteBuffer vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+        glfwSetWindowPos(window, (GLFWvidmode.width(vidmode) - width) / 2, (GLFWvidmode.height(vidmode) - height) / 2);
+        glfwMakeContextCurrent(window);
+        glfwShowWindow(window);
     }
     
     private void update() {
-        
+        glfwPollEvents();
     }
     
     private void render() {
-        
+        glfwSwapBuffers(window);
     }
     
     public static void main(String[] args) {
